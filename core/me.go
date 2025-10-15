12e24549d9330f0fd94fba1ec7eafa188dff9a81
Surawit-Ps <surawittisachai@gmail.com>
2025-10-15 17:19:05 +0700

    first hexagonal commit2

--- core/me.go
+++ core/me.go
@@ -6,5 +6,5 @@ type Me struct {
 	NickName string `json:"nick_name" bson:"nick_name"`
 	Age int `json:"age" bson:"age"`
 	EducaRecord []string `json:"educa_record" bson:"educa_record"`
-	WorkExp []string `json:"work_exp" bson:"work_exp"`
+	WorkExp []string `json:"work_exp" bson:"work_exp"` //trest
 }
